1de9bfff59a9f7bdfae1fe20e85ff2fc0daf2f72
Lee Prosser <lee.prosser@embersoft.co.uk>
2016-06-05 21:31:50 +0100

    Added notion of plugins to the system.

---
+++ src/Assets/Examples/PluginExample/AppContainer.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Examples.PluginExample.HelloWorldPlugin.components;
+using EcsRx.Unity;
+
+namespace Assets.Examples.PluginExample
+{
+    public class AppContainer : EcsRxContainer
+    {
+        public AppContainer()
+        {
+            RegisterPlugin(new HelloWorldPlugin.HelloWorldPlugin());
+        }
+
+        protected override void SetupSystems() { }
+
+        protected override void SetupEntities()
+        {
+            var defaultPool = PoolManager.GetPool();
+            var entity = defaultPool.CreateEntity();
+            entity.AddComponent<SayHelloWorldComponent>();
+        }
+    }
+}
---
+++ src/Assets/Examples/PluginExample/HelloWorldPlugin/systems/OutputHelloWorldSystem.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Examples.PluginExample.HelloWorldPlugin.components;
+using EcsRx.Entities;
+using EcsRx.Groups;
+using EcsRx.Systems;
+using UnityEngine;
+
+namespace Assets.Examples.PluginExample.HelloWorldPlugin.systems
+{
+    public class OutputHelloWorldSystem : ISetupSystem
+    {
+        public IGroup TargetGroup { get { return new Group(typeof(SayHelloWorldComponent));} }
+
+        public void Setup(IEntity entity)
+        {
+            Debug.Log(string.Format("Entity {0} Says Hello World", entity.Id));
+        }
+    }
+}
