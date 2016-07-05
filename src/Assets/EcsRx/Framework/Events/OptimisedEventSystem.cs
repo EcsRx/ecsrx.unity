81328bba47fd0f0fd0393eb73798d19f3d76e885
Lee Prosser <lee.prosser@embersoft.co.uk>
2016-07-05 09:56:25 +0100

    Added OptimisedEventSystem.cs however not sure if I will replace the current one with it yet.

---
+++ src/Assets/EcsRx/Framework/Events/OptimisedEventSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+
+namespace EcsRx.Events
+{
+    public class OptimisedEventSystem : IEventSystem
+    {
+        private readonly IDictionary<Type, IMessageBroker> _messageBrokers = new Dictionary<Type, IMessageBroker>();
+
+        public void Publish<T>(T message)
+        {
+            var typeKey = typeof (T);
+            if (!_messageBrokers.ContainsKey(typeKey))
+            { _messageBrokers.Add(typeKey, new MessageBroker()); }
+
+            _messageBrokers[typeKey].Publish(message);
+        }
+
+        public IObservable<T> Receive<T>()
+        {
+            var typeKey = typeof (T);
+            if(!_messageBrokers.ContainsKey(typeKey))
+            { _messageBrokers.Add(typeKey, new MessageBroker()); }
+
+            return _messageBrokers[typeKey].Receive<T>();
+        }
+    }
+}
