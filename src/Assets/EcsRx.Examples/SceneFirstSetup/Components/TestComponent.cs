6b259457a71301488868f144fe25fc643e9f86c9
LP <grofit1@yahoo.com>
2018-09-27 20:18:02 +0100

    minor refactors

--- src/Assets/EcsRx.Examples/SceneFirstSetup/Components/TestComponent.cs
+++ src/Assets/EcsRx.Examples/SceneFirstSetup/Components/TestComponent.cs
@@ -1,12 +1,25 @@
-﻿using EcsRx.Components;
+﻿using System;
+using EcsRx.Components;
+using UniRx;
 
 namespace EcsRx.Examples.SceneFirstSetup.Components
 {
-    public class TestComponent : IComponent
+    public class TestComponent : IComponent, IDisposable
     {
         public string Name { get; set; }
         public int Age { get; set; }
         public bool IsHappy { get; set; }
         public float Roundness { get; set; }
+        public ReactiveProperty<float> ReactiveFloat { get; }
+
+        public TestComponent()
+        {
+            ReactiveFloat = new ReactiveProperty<float>(0);
+        }
+
+        public void Dispose()
+        {
+            ReactiveFloat?.Dispose();
+        }
     }
 }
